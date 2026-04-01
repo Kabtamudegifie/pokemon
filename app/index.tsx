a2079630c6bfe78c2e8a0a59e6de550e6560b18b
Kabtamu Degifie <kabtamu@etmsoftwareplc.com>
2026-04-01 14:43:50 +0300

    feat: initial commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View className="flex items-center justify-center w-full h-full">
+      <Text className="text-gray-400 dark:text-white">Home</Text>
+    </View>
+  );
+}
---
+++ components/ui/index.ts
@@ -0,0 +1,9 @@
+export * from "./button";
+export * from "./safe-area-view";
+export * from "./text";
+export * from "./theme";
+
+export { cn } from "./utils/cn";
+export * from "./utils/platform";
+export { TextClassContext } from "./utils/text-context";
+export { useColorScheme } from "./utils/use-color-scheme";
